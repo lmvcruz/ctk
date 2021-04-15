3c717d8a1e4f177b23f2ae1a9bac894c61ed4f8a
Leandro Cruz <l.cruz@psenterprise.com>
2021-04-15 16:17:05 +0100

    [MOSAIC] Replace copyImage func to IMAGE.CopyFrom

--- demos/mosaic/main.cpp
+++ demos/mosaic/main.cpp
@@ -4,44 +4,55 @@
 #include "ctk/image/rgb_image.h"
 #include "ctk/misc/filesys.h"
 
-void copyImage(ctk::RgbImage& mosaic, ctk::RgbImage& that, 
-               int ox, int oy, int w, int h) 
-{
-    for (int x = 0; x < w; ++x) {
-        for (int y = 0; y < h; ++y) {
-            mosaic.set(ox + x, oy + y, that.get(x, y));
-        }
+void processImage(ctk::RgbImage& img, int w, int h) {
+    int iw = img.width();
+    int ih = img.height();
+    double expRatio = static_cast<double>(w) / static_cast<double>(h);
+    double diw = static_cast<double>(iw);
+    double dih = static_cast<double>(ih);
+    double curRatio = diw / dih;
+    int ox = 0;
+    int oy = 0;
+    int niw = iw;
+    int nih = ih;
+    if (curRatio >= expRatio) {
+        niw = static_cast<int>(diw * expRatio / curRatio );
+        ox = (iw - niw) / 2;
+    } else {
+        nih = static_cast<int>(dih * curRatio / expRatio );
+        oy = (ih - nih) / 2;
     }
-} 
+    img.SelfCrop(ox, oy, niw, nih);
+    img = img.resize(w, h);
+}
 
-void createMosaic() {
-    std::vector<std::string> filters = {"png", "PNG", "jpg", "jpeg", "JPEG", "JPG"};
-    std::vector<std::string> files = ctk::listFilesContainingAnyExpressions(
-                                            "../../../../imgdb/dogs",
-                                            filters);
-    //
+void createRegularMosaic(std::vector<std::string>& imgDb) {
+    ctk::RgbImage aux;
     ctk::RgbImage mosaic;
-    int cs = 300;
-    int gs = 3;
-    int fs = 3;
-    std::cout << fs + gs * (cs + fs) << " " << fs + gs * (cs + fs) << std::endl;
-    mosaic.Create(fs + gs * (cs + fs), fs + gs * (cs + fs));
+    int cw = 300;
+    int ch = 200;
+    int gw = 4;
+    int gh = 3;
+    int fs = 10;
+    mosaic.Create(fs + gw * (cw + fs), fs + gh * (ch + fs));
     mosaic.Fill(255, 255, 255);
-    for (int cx = 0; cx < gs; ++cx) {
-        for (int cy = 0; cy < gs; ++cy) {
-            int ox = fs + cx * cs;
-            int oy = fs + cy * cs;
-            ctk::RgbImage that;
-            std::cout << files[cx * gs + cy] << std::endl;
-            that.Open(files[cx * gs + cy]);
-            that = that.resize(cs, cs);
-            copyImage(mosaic, that, ox, oy, cs, cs);
+    for (int cx = 0; cx < gw; ++cx) {
+        int ox = fs + cx * (cw + fs);
+        for (int cy = 0; cy < gh; ++cy) {
+            int oy = fs + cy * (ch + fs);
+            aux.Open(imgDb[cx * gw + cy]);
+            processImage(aux, cw, ch);
+            mosaic.CopyFrom(aux, ox, oy, 0, 0, cw, ch);
         }
     }
     mosaic.Save("test.png");
 }
 
 int main(int argc, char *argv[]) {
-    createMosaic();
+    std::vector<std::string> filters = {"png", "PNG", "jpg", "jpeg", "JPEG", "JPG"};
+    std::vector<std::string> imgDb = 
+                            ctk::listFilesContainingAnyExpressions(
+                                        "../../../../imgdb/dogs", filters);
+    createRegularMosaic(imgDb);
     return 0;
 }
